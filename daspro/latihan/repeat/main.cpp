b6a05c717d00edc5f18deaade5f4d81d20598d9d
dika hp <111202214679@msh.dinus.ac.id>
2022-11-25 21:38:43 +0700

    10

---
+++ daspro/latihan/repeat/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    char j;
+
+    while(j!='y'){
+        cout<<"apakan anda sudah bangun(y/t) : ";
+        cin>>j;
+        n=3;
+        while(n>0){
+        n--;
+        cout<<"bangun\n";
+    }
+    }
+
+}
